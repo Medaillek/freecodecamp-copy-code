cc958ab490ad6c87acfb478399aeee61b81d08e3
Medaillek <95227250+Medaillek@users.noreply.github.com>
2022-09-26 18:35:10 +0200

    File update

--- content-script.js
+++ content-script.js
@@ -1,36 +1,7 @@
-function waitForElm(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
-
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
-};
-
-waitForElm('#description').then( ()=> {
-    let code_elem = document.getElementsByTagName('code');
-
-    for (elem of code_elem) {
-        elem.classList = `jqsdjfhe`
-    }
-})
-
 document.addEventListener("click", (e) =>{
-    if(e.path[0].className == 'jqsdjfhe') {
+    if(e.target.localName == 'code') {
         console.log(e.target.innerText)
         navigator.clipboard.writeText(e.target.innerText);
     }
-
 })
 
